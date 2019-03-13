ddd2f6ffa0a0bcdd6a91c8036c94a459aa3782b1
SHKnudsen <sylvesterknudsen@gmail.com>
2019-03-13 17:44:54 +0000

    HelperFunctions And Openess Node

--- GenerativeToolkit/HelperFunctions/DeskFunctions.cs
+++ GenerativeToolkit/HelperFunctions/DeskFunctions.cs
@@ -1,31 +1,36 @@
-﻿using DSGeo = Autodesk.DesignScript.Geometry;
+﻿using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Architecture;
+using Revit.Elements;
 
 namespace GenerativeToolkit.HelperFunctions
 {
     class DeskFunctions
     {
+        #region Get room boundaries as Polygons
         [IsVisibleInDynamoLibrary(false)]
-        public static List<DSGeo.Polygon> PolygonsFromRooms(List<Room> rooms)
+        public static List<Polygon> PolygonsFromRooms(List<Room> rooms)
         {
             rooms = FilterRooms(rooms);
+            List<Polygon> boundaryPolygon = new List<Polygon>();
             foreach (Room room in rooms)
             {
-                IList<IList<BoundarySegment>> segments = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
-                List<DSGeo.Polygon> boundaryPolygon = BoundaryPolygon(segments);
-
+                IEnumerable<IEnumerable<Curve>> segments = room.CoreBoundary;
+                boundaryPolygon.AddRange(BoundaryPolygon(segments));
             }
+            List<Polygon> roomPolygons = CombineIntersectingRooms(boundaryPolygon);
+            boundaryPolygon.ForEach(x => x.Dispose());
+
+            return roomPolygons;
         }
+        #endregion
 
-
-
+        #region Filter unplaced Rooms 
+        [IsVisibleInDynamoLibrary(false)]
         private static List<Room> FilterRooms(List<Room> rooms)
         {
             List<Room> filteredRooms = new List<Room>();
@@ -39,36 +44,67 @@ namespace GenerativeToolkit.HelperFunctions
             }
             return filteredRooms;
         }
+        #endregion
 
-        private static List<DSGeo.Polygon> BoundaryPolygon(IList<IList<BoundarySegment>> boundarySegments)
+        #region Get all rooms Boundary Polygon
+        [IsVisibleInDynamoLibrary(false)]
+        private static List<Polygon> BoundaryPolygon(IEnumerable<IEnumerable<Curve>> boundarySegments)
         {
-            List<DSGeo.Polygon> polygons = new List<DSGeo.Polygon>();  
-            foreach (IList<BoundarySegment> segmentList in boundarySegments)
+            List<Polygon> polygons = new List<Polygon>();  
+            foreach (IList<Curve> segmentList in boundarySegments)
             {          
-                List<DSGeo.Point> boundaryPoints = new List<DSGeo.Point>();  
-                foreach (BoundarySegment segment in segmentList)
+                List<Point> boundaryPoints = new List<Point>();  
+                foreach (Curve segment in segmentList)
                 {
-                    double x = segment.GetCurve().GetEndPoint(0).X;
-                    double y = segment.GetCurve().GetEndPoint(0).Y;
-                    double z = segment.GetCurve().GetEndPoint(0).Z;
-                    boundaryPoints.Add(DSGeo.Point.ByCoordinates(x, y, z));
+                    double x = segment.StartPoint.X;
+                    double y = segment.StartPoint.Y;
+                    boundaryPoints.Add(Point.ByCoordinates(x, y));
+                    segment.Dispose();
                 }
-                polygons.Add(DSGeo.Polygon.ByPoints(boundaryPoints));
+                polygons.Add(Polygon.ByPoints(boundaryPoints));
                 boundaryPoints.ForEach(x => x.Dispose());
             }
+
             return polygons;
         }
+        #endregion
 
-        private static List<DSGeo.Polygon> CombineIntersectingRooms(List<DSGeo.Polygon> polygons)
+        #region Combine intersecting rooms only seperated with a room seperation line
+        [IsVisibleInDynamoLibrary(false)]
+        private static List<Polygon> CombineIntersectingRooms(List<Polygon> polygons)
         {
-            List<DSGeo.Solid> solidSurfs = new List<DSGeo.Solid>();
-            foreach (DSGeo.Polygon polygon in polygons)
+            Plane plane = Plane.ByOriginNormal(Point.ByCoordinates(0, 0, 0), Vector.ByCoordinates(0, 0, 1));
+            List<Solid> solidSurfs = new List<Solid>();
+            foreach (Polygon polygon in polygons)
             {
-                DSGeo.Solid solid = DSGeo.Surface.ByPatch(polygon).Thicken(1);
+                Solid solid = Surface.ByPatch(polygon).Thicken(1);
                 solidSurfs.Add(solid);
             }
-            DSGeo.Solid solidUnion = DSGeo.Solid.ByUnion(solidSurfs);
+            Solid solidUnion = Solid.ByUnion(solidSurfs);
+            Autodesk.DesignScript.Geometry.Geometry[] roomSurfs = solidUnion.Intersect(plane);
 
+            List<Polygon> spacePolygons = new List<Polygon>();
+            foreach (Surface surf in roomSurfs)
+            {
+                Curve[] perimeterCrvs = surf.PerimeterCurves();
+                List<Point> polygonPoints = new List<Point>();
+                foreach (Curve crv in perimeterCrvs)
+                {
+                    polygonPoints.Add(crv.StartPoint);
+                    crv.Dispose();
+                }
+                spacePolygons.Add(Polygon.ByPoints(polygonPoints));
+                polygonPoints.ForEach(x => x.Dispose());
+                surf.Dispose();
+            }
+
+            // Dispose Dynamo Geometry
+            solidSurfs.ForEach(x => x.Dispose());
+            solidUnion.Dispose();
+            plane.Dispose();
+
+            return spacePolygons;
         }
+        #endregion
     }
 }
---
+++ GenerativeToolkit/Relationships/Openess.cs
@@ -0,0 +1,99 @@
+﻿using Autodesk.DesignScript.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using Revit.Elements;
+using Autodesk.Revit.DB;
+
+namespace GenerativeToolkit.Relationships
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class Openess
+    {
+        internal Openess()
+        {
+
+        }
+
+        [IsVisibleInDynamoLibrary(true)]
+        public static List<double> DeskOpeness(List<Room> rooms, List<Revit.Elements.FamilyInstance> elements, List<double> deskWidth, List<double> deskLength, double offset)
+        {
+            List<Polygon> roomPolygons = HelperFunctions.DeskFunctions.PolygonsFromRooms(rooms);
+            List<double> openessScore = new List<double>();
+            for (int i = 0; i < elements.Count; i++)
+            {
+                List<Autodesk.DesignScript.Geometry.Line> intersectionLines = IntersectionLine(elements[i], deskWidth[i], deskLength[i], offset);
+                openessScore.Add(OpenessScore(intersectionLines, roomPolygons));
+                intersectionLines.ForEach(line => line.Dispose());
+            }
+            roomPolygons.ForEach(poly => poly.Dispose());
+
+            return openessScore;
+        }
+
+        private static List<Autodesk.DesignScript.Geometry.Line> IntersectionLine(Revit.Elements.FamilyInstance element, double deskWidth, double deskLength, double offset)
+        {
+            Autodesk.DesignScript.Geometry.Point locationPoint = (Autodesk.DesignScript.Geometry.Point) element.GetLocation();
+            double width = deskWidth + offset;
+            double length = deskLength + offset;
+
+            double rotation = ElementRoation(element);
+            Vector x = Vector.XAxis().Rotate(Vector.ZAxis(), -rotation);
+            Vector y = Vector.YAxis().Rotate(Vector.ZAxis(), -rotation);
+
+            Autodesk.DesignScript.Geometry.Line rightLine = Autodesk.DesignScript.Geometry.Line.ByStartPointDirectionLength(locationPoint, x, width/2);
+            Autodesk.DesignScript.Geometry.Line topLine = Autodesk.DesignScript.Geometry.Line.ByStartPointDirectionLength(locationPoint, y, length / 2);
+            Autodesk.DesignScript.Geometry.Line leftLine = Autodesk.DesignScript.Geometry.Line.ByStartPointDirectionLength(locationPoint, x, -width / 2);
+            Autodesk.DesignScript.Geometry.Line bottomLine = Autodesk.DesignScript.Geometry.Line.ByStartPointDirectionLength(locationPoint, y, -length / 2);
+
+            List<Autodesk.DesignScript.Geometry.Line> lines = new List<Autodesk.DesignScript.Geometry.Line> { rightLine, topLine, leftLine, bottomLine };
+            return lines;
+        }
+
+        private static double ElementRoation(Revit.Elements.FamilyInstance element)
+        {
+            Autodesk.Revit.DB.FamilyInstance unwrappedElement = (Autodesk.Revit.DB.FamilyInstance) element.InternalElement;
+            double x = unwrappedElement.HandOrientation.X;
+            double y = unwrappedElement.HandOrientation.Y;
+
+            double rotation;
+            if (y >= 0)
+            {
+                rotation = Math.Acos(x) * (180 / Math.PI);
+            }
+            else
+            {
+                rotation = (Math.Acos(x) * (180 / Math.PI)) + (2 * (180 - (Math.Acos(x) * (180 / Math.PI))));
+            }
+
+            if (rotation == 0)
+            {
+                rotation = 0;
+            }
+            else
+            {
+                rotation = 360 - rotation;
+            }
+            return rotation;
+        }
+
+        private static double OpenessScore(List<Autodesk.DesignScript.Geometry.Line> lines, List<Polygon> polygons)
+        {
+            double score = 0.0;
+            foreach (var line in lines)
+            {
+                foreach (var polygon in polygons)
+                {
+                    if (line.DoesIntersect(polygon))
+                    {
+                        score += 0.25;
+                    }
+                }
+            }
+            return score;
+        }
+    }
+}
