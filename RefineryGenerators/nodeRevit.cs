da1d99f505df9b6e0694630b789ca595e4d35d0f
Trygve <trygve@provingground.io>
2019-03-15 12:03:52 -0500

    Add CreateRevitMass node.

--- RefineryGenerators/nodeRevit.cs
+++ RefineryGenerators/nodeRevit.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Autodesk.DesignScript.Runtime;
 using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+using Revit.GeometryConversion;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
 
 namespace Revit
 {
+    /// <summary>
+    /// ElementCreation description.
+    /// </summary>
     public static class ElementCreation
     {
+        internal static Autodesk.Revit.DB.Document Document => DocumentManager.Instance.CurrentDBDocument;
+
         /// <summary>
         /// Creates a Revit floors from building masser
         /// </summary>
@@ -32,19 +36,48 @@ namespace Revit
         /// <summary>
         /// Creates a Revit mass as a direct shape from building masser
         /// </summary>
-        /// <param name="Mass">A polysurface mass</param>
-        /// <param name="Category">A category for the mass</param>
-        /// <returns></returns>
+        /// <param name="Mass">The building mass.</param>
+        /// <param name="CategoryName">A category for the mass.</param>
+        /// <returns name="MassElement">Revit DirectShape element.</returns>
         /// <search>refinery</search>
         [MultiReturn(new[] { "MassElement" })]
-        public static Dictionary<string, object> CreateRevitMass(double Mass, string Category)
+        public static Dictionary<string, object> CreateRevitMass(Solid Mass, string CategoryName = "Mass")
         {
-            List<Surface> elements = null;
+            Autodesk.Revit.DB.DirectShape shape = null;
+            
+            if (Mass == null)
+            {
+                throw new ArgumentNullException(nameof(Mass));
+            }
+
+            var category = Document.Settings.Categories.get_Item(CategoryName);
+
+            TransactionManager.Instance.EnsureInTransaction(Document);
+
+            shape = Autodesk.Revit.DB.DirectShape.CreateElement(Document, category.Id);
+
+            try
+            {
+                shape.SetShape(new[] { DynamoToRevitBRep.ToRevitType(Mass) });
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    shape.SetShape(Mass.ToRevitType());
+                }
+                catch
+                {
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+
+            TransactionManager.Instance.TransactionTaskDone();
 
             // return a dictionary
             return new Dictionary<string, object>
             {
-                {"MassElement", elements},
+                {"MassElement", shape},
             };
         }
     }
