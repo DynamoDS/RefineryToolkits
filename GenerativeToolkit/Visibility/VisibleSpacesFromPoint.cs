1c59a09278c7e500c985b617dd56bbcfbbae40ed
SHKnudsen <sylvesterknudsen@gmail.com>
2019-03-15 12:59:35 +0000

    VisibleSpaces Node
    Added visible spaces from desk node, and cleaned up Graphical implementation. Still issues with conflict between GenerativeToolkit and Graphical package

--- GenerativeToolkit/Visibility/VisibleSpacesFromPoint.cs
+++ GenerativeToolkit/Visibility/VisibleSpacesFromPoint.cs
@@ -9,13 +9,41 @@ using Revit.Elements;
 
 namespace GenerativeToolkit.Visibility
 {
-    class VisibleSpacesFromPoint
+    [IsVisibleInDynamoLibrary(false)]
+    public class VisibleSpacesFromPoint
     {
-        [MultiReturn(new[] { "visGraph", "factors" })]
-        public static Dictionary<string, object> VisibleSpaces(List<Room> rooms, List<Point> points )
+        [IsVisibleInDynamoLibrary(true)]
+        public static List<Room> VisibleRoomsFromDesk(List<Room> rooms, Point point )
         {
+            List<Polygon> spacePolygons = HelperFunctions.DeskFunctions.PolygonsFromSpaces(rooms);
+            List<Polygon> roomPolygons = HelperFunctions.DeskFunctions.RoomPolygons;
+
+            Surface isovist = MakeIsovist(spacePolygons, point);
+            List<Room> visibleRooms = VisibleSpaces(isovist, roomPolygons, rooms);
+            return visibleRooms;
 
         }
 
+        private static Surface MakeIsovist(List<Polygon> polygons, Point point)
+        {
+            GraphicalDynamo.Graphs.BaseGraph baseGraph = GraphicalDynamo.Graphs.BaseGraph.ByPolygons(polygons);
+            Surface isovist = GraphicalDynamo.Graphs.BaseGraph.IsovistFromPoint(baseGraph, point);
+            return isovist;
+        }
+
+        private static List<Room> VisibleSpaces(Surface isovist, List<Polygon> roomPolygons, List<Room> rooms)
+        {
+            List<Room> visibleRooms = new List<Room>();
+
+            for (int i = 0; i < rooms.Count-1; i++)
+            {
+                if (isovist.DoesIntersect(roomPolygons[i]))
+                {
+                    visibleRooms.Add(rooms[i]);
+                }
+            }
+
+            return visibleRooms;
+        } 
     }
 }
