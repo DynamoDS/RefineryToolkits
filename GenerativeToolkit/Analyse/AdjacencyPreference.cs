7e5f7ccdfb56c8aeb4dd8950f2894f9ad53dc402
SHKnudsen <sylvesterknudsen@gmail.com>
2019-03-25 09:33:13 +0000

    Changed to static classes

--- GenerativeToolkit/Analyse/AdjacencyPreference.cs
+++ GenerativeToolkit/Analyse/AdjacencyPreference.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GenerativeToolkit.Analyse
 {
-    class AdjacencyPreference
+    static class AdjacencyPreference
     {
     }
 }
--- GenerativeToolkit/Analyse/SpaceNavigation.cs
+++ GenerativeToolkit/Analyse/SpaceNavigation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GenerativeToolkit.Analyse
 {
-    class SpaceNavigation
+    static class SpaceNavigation
     {
     }
 }
