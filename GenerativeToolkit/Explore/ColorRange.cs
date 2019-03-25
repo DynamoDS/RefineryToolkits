cdf19e2e95c9180b9283da7823b22fdc1ad68d19
SHKnudsen <sylvesterknudsen@gmail.com>
2019-03-25 10:42:41 +0000

    Update ColorRange Node

--- GenerativeToolkit/Explore/ColorRange.cs
+++ GenerativeToolkit/Explore/ColorRange.cs
@@ -8,79 +8,86 @@ using System.Threading.Tasks;
 
 namespace Autodesk.GenerativeToolkit.Explore
 {
-    class ColorRange
+    [IsVisibleInDynamoLibrary(false)]
+    public static class ColorRange
     {
-        // Colors from https://www.materialpalette.com/colors
         [IsVisibleInDynamoLibrary(true)]
         public static List<Color> ContrastyColorRange(int amount, double correctionFactor)
         {
-            if (amount <= 19)
+            if (amount > 19)
             {
-                Random rnd = new Random();
-                List<string> usedColors = new List<string>();
-                List<Color> colors = new List<Color>();
+                throw new Exception("Maximum number of colours supported right now is 19");
+            }
+            Random rnd = new Random();
+            List<int> indexRanges = Enumerable.Range(0, amount).ToList();
+            List<int> shuffledIndexes = indexRanges.OrderBy(x => rnd.Next()).ToList();
 
-                Dictionary<string, List<int>> colorDict = new Dictionary<string, List<int>>
+            List<Color> colors = new List<Color>();
+            foreach (int i in shuffledIndexes)
+            {
+                Color color = ColorPalette[i];
+                int red;
+                int green;
+                int blue;
+
+                if (correctionFactor <= 0)
                 {
-                    ["Red"] = new List<int>() { 244, 67, 54 },
-                    ["Pink"] = new List<int>() { 233, 30, 99 },
-                    ["Purple"] = new List<int>() { 156, 39, 176 },
-                    ["Deep Purple"] = new List<int>() { 103, 58, 183 },
-                    ["Indigo"] = new List<int>() { 63, 81, 181 },
-                    ["Blue"] = new List<int>() { 33, 150, 243 },
-                    ["Light Blue"] = new List<int>() { 3, 169, 244 },
-                    ["Cyan"] = new List<int>() { 0, 188, 212 },
-                    ["Teal"] = new List<int>() { 0, 150, 136 },
-                    ["Green"] = new List<int>() { 76, 175, 80 },
-                    ["Light Green"] = new List<int>() { 139, 195, 74 },
-                    ["Lime"] = new List<int>() { 80, 86, 22 },
-                    ["Yellow"] = new List<int>() { 255, 235, 59 },
-                    ["Amber"] = new List<int>() { 255, 193, 7 },
-                    ["Orange"] = new List<int>() { 255, 152, 0 },
-                    ["Deep Orange"] = new List<int>() { 255, 87, 34 },
-                    ["Brown"] = new List<int>() { 121, 85, 72 },
-                    ["Grey"] = new List<int>() { 158, 158, 158 },
-                    ["Blue Grey"] = new List<int>() { 96, 125, 139 }
-                };
-
-                for (int i = 0; i < amount; i++)
-                {
-                    var randomColor = colorDict.ElementAt(rnd.Next(0, colorDict.Count));
-                    double red = randomColor.Value[0];
-                    double green = randomColor.Value[1];
-                    double blue = randomColor.Value[2];
-
-                    Color color = DSCore.Color.ByARGB(r: randomColor.Value[0], g: randomColor.Value[1], b: randomColor.Value[2]);
-                    if (!usedColors.Contains(randomColor.Key))
-                    {
-                        usedColors.Add(randomColor.Key);
-                        if (correctionFactor <= 0)
-                        {
-                            red *= 1 + correctionFactor;
-                            green *= 1 + correctionFactor;
-                            blue *= 1 + correctionFactor;
-                        }
-                        else
-                        {
-                            red = (255 - red) * correctionFactor + red;
-                            green = (255 - green) * correctionFactor + green;
-                            blue = (255 - blue) * correctionFactor + blue;
-                        }
-
-                        colors.Add(Color.ByARGB(r: (int)red, g: (int)green, b: (int)blue));
-                    }
-                    else
-                    {
-                        i -= 1;
-                    }
-
+                    red = Convert.ToInt32(color.Red * (1 + correctionFactor));
+                    green = Convert.ToInt32(color.Green * (1 + correctionFactor));
+                    blue = Convert.ToInt32(color.Blue * (1 + correctionFactor));
                 }
-                return colors;
+                else
+                {
+                    red = Convert.ToInt32((255 - color.Red) * correctionFactor + color.Red);
+                    green = Convert.ToInt32((255 - color.Green) * correctionFactor + color.Green);
+                    blue = Convert.ToInt32((255 - color.Blue) * correctionFactor + color.Blue);
+                }
+                colors.Add(Color.ByARGB(r: red, g: green, b: blue));            
             }
-            else
-            {
-                return null;
-            }
+            return colors;
         }
+
+        // Colors from https://www.materialpalette.com/colors
+        private static readonly List<Color> ColorPalette = new List<Color>()
+        {
+            //Red
+            Color.ByARGB(0, 244, 67, 54),
+            //Pink
+            Color.ByARGB(0, 233, 30, 99),
+            //Purple
+            Color.ByARGB(0, 156, 39, 176),
+            //Deep Purple
+            Color.ByARGB(0, 103, 58, 183),
+            //Indigo
+            Color.ByARGB(0, 63, 81, 181),
+            //Blue
+            Color.ByARGB(0, 33, 150, 243),
+            //Light Blue
+            Color.ByARGB(0, 3, 169, 244),
+            //Cyan
+            Color.ByARGB(0, 0, 188, 212),
+            //Teal
+            Color.ByARGB(0, 0, 150, 136),
+            //Green
+            Color.ByARGB(0, 76, 175, 80),
+            //Light Green
+            Color.ByARGB(0, 139, 195, 74),
+            //Lime
+            Color.ByARGB(0, 80, 86, 22),
+            //Yellow
+            Color.ByARGB(0, 255, 235, 59),
+            //Amber
+            Color.ByARGB(0, 255, 193, 7),
+            //Orange
+            Color.ByARGB(0, 255, 152, 0),
+            //Deep Orange
+            Color.ByARGB(0, 255, 87, 34),
+            //Brown
+            Color.ByARGB(0, 121, 85, 72),
+            //Grey
+            Color.ByARGB(0, 158, 158, 158),
+            //Blue Grey
+            Color.ByARGB(0, 96, 125, 139)
+        };
     }
 }
