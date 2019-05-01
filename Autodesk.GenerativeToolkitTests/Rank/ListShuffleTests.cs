026d9dc4b228c1e903b02eda660af88fac62cd03
SHKnudsen <sylvesterknudsen@gmail.com>
2019-05-01 09:48:57 +0100

    Update to Rank Unit test

--- Autodesk.GenerativeToolkitTests/Rank/ListShuffleTests.cs
+++ Autodesk.GenerativeToolkitTests/Rank/ListShuffleTests.cs
@@ -11,18 +11,45 @@ namespace Autodesk.GenerativeToolkit.Rank.Tests
     [TestFixture()]
     public class ListShuffleTests
     {
-        // Checks if the input lists values are suffled in a different way
-        // when seed changes
-        [Test()]
-        public void SeededShuffleTest()
+        private List<int> sampleList;
+
+        [SetUp]
+        public void BeforeTest()
         {
-            List<int> sampleList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var seedOne = ListShuffle.SeededShuffle(sampleList);
-            Assert.AreNotEqual(sampleList, seedOne);
+            sampleList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
 
-            var seedTwo = ListShuffle.SeededShuffle(sampleList, 2);
-            Assert.AreNotEqual(seedOne, seedTwo);
+        /// <summary>
+        /// Checks if the input lists values are suffled in a different way
+        /// when seed changes
+        /// </summary>
+        [Test()]
+        public void ShuffledListIsDifferentFromOriginalListTest()
+        {
+            var listShuffle1 = ListShuffle.SeededShuffle(sampleList);
+            Assert.AreNotEqual(sampleList, listShuffle1);     
+        }
 
+        /// <summary>
+        /// Checks if the same list shuffled with two different seeds are different from each other
+        /// </summary>
+        [Test]
+        public void DifferentSeedReturnsDifferentListTest()
+        {
+            var listShuffle1 = ListShuffle.SeededShuffle(sampleList, 1);
+            var listShuffle2 = ListShuffle.SeededShuffle(sampleList, 2);
+            Assert.AreNotEqual(listShuffle1, listShuffle2);
+        }
+
+        /// <summary>
+        /// Checks if the same list shuffled with the same seed returns the same
+        /// </summary>
+        [Test]
+        public void SameSeedReturnsSameListTest()
+        {
+            var listShuffle1 = ListShuffle.SeededShuffle(sampleList, 3);
+            var listShuffle2 = ListShuffle.SeededShuffle(sampleList, 3);
+            Assert.AreEqual(listShuffle1, listShuffle2);
         }
     }
 }
