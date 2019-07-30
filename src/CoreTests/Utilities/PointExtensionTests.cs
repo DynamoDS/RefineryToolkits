d5c33c45d4287fff37bd51286132babedcc245c4
Radu Gidei <radugidei@gmail.com>
2019-07-30 19:19:43 +0100

    add colinearity tests for 3,4,5 points

--- src/CoreTests/Utilities/PointExtensionTests.cs
+++ src/CoreTests/Utilities/PointExtensionTests.cs
@@ -28,13 +28,52 @@ namespace Autodesk.RefineryToolkits.Core.Utillites.Tests
         }
 
         [Test]
-        public void AreColinearTest()
+        public void Colinear_CanIdentifyNonColinear()
         {
-            var colinear = this.ColinearPoints.AreColinear();
             var noncolinear = this.NonColinearPoints.AreColinear();
-
-            Assert.IsTrue(colinear);
             Assert.IsFalse(noncolinear);
         }
+
+
+        [Test]
+        public void Colinear_CanIdentify3Points()
+        {
+            // Arrange
+            var threePoints = new List<Point>(this.ColinearPoints);
+            threePoints.RemoveAt(threePoints.Count - 1);
+
+            // Act
+            var colinear = threePoints.AreColinear();
+
+            // Assert
+            Assert.AreEqual(3, threePoints.Count);
+            Assert.IsTrue(colinear);
+        }
+
+        [Test]
+        public void Colinear_CanIdentify4Points()
+        {
+            var colinear = this.ColinearPoints.AreColinear();
+            Assert.IsTrue(colinear);
+            Assert.AreEqual(3, ColinearPoints.Count);
+        }
+
+        [Test]
+        public void Colinear_CanIdentify5Points()
+        {
+            // Arrange
+            var fivePoints = new List<Point>(this.ColinearPoints);
+            fivePoints.Add(Point.ByCoordinates(250, 600));
+            fivePoints.Add(Point.ByCoordinates(350, 600));
+
+            // Act
+            var colinear = fivePoints.AreColinear();
+
+            // Assert
+            Assert.IsTrue(colinear);
+            Assert.AreEqual(5, fivePoints.Count);
+        }
+
+
     }
 }
