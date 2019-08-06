fbed98dcacf310870f87acb9924296552dd136ad
Radu Gidei <radugidei@gmail.com>
2019-08-06 15:06:21 +0100

    refine & add new tests for bin packing

--- tests/Autodesk.RefineryToolkits.SpacePlanningTests/Generate/BinPacking2DTests.cs
+++ tests/Autodesk.RefineryToolkits.SpacePlanningTests/Generate/BinPacking2DTests.cs
@@ -1,10 +1,7 @@
 ﻿using Autodesk.DesignScript.Geometry;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TestServices;
 
 namespace Autodesk.RefineryToolkits.SpacePlanning.Generate.Tests
@@ -13,10 +10,37 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Generate.Tests
     public class BinPacking2DTests : GeometricTestBase
     {
         [Test]
-        public void PackTest()
+        public void OneBin_CanPackWithNoLeftovers()
         {
-            Rectangle bin = Rectangle.ByWidthLength(50,50);
-            List<Rectangle> items = new List<Rectangle>
+            var bins = new List<Rectangle> { Rectangle.ByWidthLength(50, 50) };
+            var items = new List<Rectangle>
+            {
+                Rectangle.ByWidthLength(20,23),
+                Rectangle.ByWidthLength(12,30),
+                Rectangle.ByWidthLength(33,20),
+                Rectangle.ByWidthLength(12,22)
+            };
+
+            Dictionary<string, object> result = BinPacking.Pack2D(items, bins, BinPacking.PlacementMethods.BestShortSideFits);
+            Assert.IsTrue(result.Keys.Contains("packedRectangles"));
+            Assert.IsTrue(result.Keys.Contains("remainRectangles"));
+            Assert.IsTrue(result.Keys.Contains("packedIndices"));
+
+            var packedRectangles = (List<Rectangle>)result["packedRectangles"];
+            Assert.AreEqual(4, packedRectangles.Count);
+
+            var remainItems = (List<Rectangle>)result["remainRectangles"];
+            Assert.AreEqual(0, remainItems.Count);
+
+            var packedIndices = (List<int>)result["packedIndices"];
+            Assert.AreEqual(packedIndices, new List<int> { 0, 1, 2, 3 });
+        }
+
+        [Test]
+        public void OneBin_CanPackAndHaveLeftovers()
+        {
+            var bins = new List<Rectangle> { Rectangle.ByWidthLength(50, 50) };
+            var items = new List<Rectangle>
             {
                 Rectangle.ByWidthLength(20,23),
                 Rectangle.ByWidthLength(12,30),
@@ -26,7 +50,7 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Generate.Tests
                 Rectangle.ByWidthLength(15,15)
             };
 
-            var result = BinPacking.Pack2D(items, bin, BinPacking.PlacementMethods.BestShortSideFits);
+            Dictionary<string, object> result = BinPacking.Pack2D(items, bins, BinPacking.PlacementMethods.BestShortSideFits);
             Assert.IsTrue(result.Keys.Contains("packedRectangles"));
             Assert.IsTrue(result.Keys.Contains("remainRectangles"));
             Assert.IsTrue(result.Keys.Contains("packedIndices"));
@@ -40,5 +64,39 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Generate.Tests
             var packedIndices = (List<int>)result["packedIndices"];
             Assert.AreEqual(packedIndices, new List<int> { 0, 1, 2, 4 });
         }
+
+        [Test]
+        public void MultiBin_CanPackAndHaveLeftovers()
+        {
+            var bins = new List<Rectangle> {
+                Rectangle.ByWidthLength(30, 30),
+                Rectangle.ByWidthLength(30, 30),
+                Rectangle.ByWidthLength(30, 30),
+            };
+            var items = new List<Rectangle>
+            {
+                Rectangle.ByWidthLength(20,23),
+                Rectangle.ByWidthLength(12,30),
+                Rectangle.ByWidthLength(33,20),
+                Rectangle.ByWidthLength(32,31),
+                Rectangle.ByWidthLength(12,22),
+                Rectangle.ByWidthLength(15,15)
+            };
+
+            Dictionary<string, object> result = BinPacking.Pack2D(items, bins, BinPacking.PlacementMethods.BestShortSideFits);
+            Assert.IsTrue(result.Keys.Contains("packedRectangles"));
+            Assert.IsTrue(result.Keys.Contains("remainRectangles"));
+            Assert.IsTrue(result.Keys.Contains("packedIndices"));
+
+            var packedRectangles = (List<Rectangle>)result["packedRectangles"];
+            Assert.AreEqual(6, packedRectangles.Count);
+
+            var remainItems = (List<Rectangle>)result["remainRectangles"];
+            Assert.AreEqual(0, remainItems.Count);
+
+            var packedIndices = (List<int>)result["packedIndices"];
+            Assert.AreEqual(packedIndices, new List<int> { 0, 1, 2, 3, 4, 5 });
+        }
+
     }
 }
