4709423d5ff2edaba36a3e935682bbdbbcf8a3ed
Radu Gidei <radugidei@gmail.com>
2019-08-14 18:16:28 +0100

    merge ViewsToOutside into Visibility

--- src/SpacePlanning/Analyze/VisiblePoints.cs
+++ src/SpacePlanning/Analyze/VisiblePoints.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.DesignScript.Runtime;
+﻿using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Analyze
     {
         private const string scoreOutputPort = "score";
         private const string geometryOutputPort = "visiblePoints";
+        private const string geometryOutputPortViews = "segments";
 
         /// <summary>
         /// Calculates the visible points out of a list of sample points from a given origin.
@@ -54,6 +56,58 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Analyze
             };
         }
 
+
+        /// <summary>
+        /// Calculates the view to outside from a given point based on a 360 degree ratio.
+        /// Returns a number from 0-1 where 0 indicates no points are visible and 1 indicates all points are visible.
+        /// </summary>
+        /// <param name="boundary">Polygon(s) enclosing all internal Polygons</param>
+        /// <param name="internals">List of Polygons representing internal obstructions</param>
+        /// <param name="viewSegments">Line segments representing the views to outside</param>
+        /// <param name="origin">Origin point to measure from</param>
+        /// <returns>precentage of 360 view that is to the outside</returns>
+        [MultiReturn(new[] { scoreOutputPort, geometryOutputPortViews })]
+        public static Dictionary<string, object> ByLineSegments(
+            List<Curve> viewSegments,
+            Point origin,
+            List<Polygon> boundary,
+            [DefaultArgument("[]")] List<Polygon> internals)
+        {
+            Surface isovist = Isovist.FromPoint(boundary, internals, origin);
+
+            List<Curve> lines = new List<Curve>();
+            double outsideViewAngles = 0;
+            foreach (Curve segment in viewSegments)
+            {
+                Geometry[] intersectSegment = isovist.Intersect(segment);
+                if (intersectSegment != null)
+                {
+                    foreach (Curve seg in intersectSegment)
+                    {
+                        lines.Add(seg);
+                        var vec1 = Vector.ByTwoPoints(origin, seg.StartPoint);
+                        var vec2 = Vector.ByTwoPoints(origin, seg.EndPoint);
+                        outsideViewAngles += vec1.AngleWithVector(vec2);
+                        vec1.Dispose();
+                        vec2.Dispose();
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            isovist.Dispose();
+            double score = outsideViewAngles / 360;
+
+            return new Dictionary<string, object>()
+            {
+                {scoreOutputPort, score },
+                {geometryOutputPort, lines }
+            };
+        }
+
+        #region Helpers
         private static DSGeom.Polygon IsovistPolygon(
             List<DSGeom.Polygon> boundary,
             List<DSGeom.Polygon> obstacles,
@@ -86,5 +140,7 @@ namespace Autodesk.RefineryToolkits.SpacePlanning.Analyze
             }
             return polygon;
         }
+
+        #endregion
     }
 }
